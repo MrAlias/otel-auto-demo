f6e6720d164722415c0bbb3e88a5627050a1cec9
Tyler Yahn <codingalias@gmail.com>
2024-08-07 16:56:48 -0700

    Instrument frontend service with OpenTelemetry
    - Add manual-instrumentation for tracing to all handled business logic
      in routes using the OpenTelemetry API.
    - Use the otelhttp instrumentation library to include generic HTTP
      instrumentation.
    - Add setup for the OpenTelemetry default SDK during startup.

--- frontend/server.go
+++ frontend/server.go
@@ -10,15 +10,22 @@ import (
 	"net/http"
 	"strconv"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+
 	"github.com/MrAlias/otel-auto-demo/user"
 )
+
+const scope = "github.com/MrAlias/otel-auto-demo/frontend"
 
 type serviceKeyType int
 
 const userKey serviceKeyType = 0
 
 func newServer(ctx context.Context, listenAddr, userAddr string) *http.Server {
-	client := user.NewClient(http.DefaultClient, userAddr)
+	client := user.NewClient(otelhttp.DefaultClient, userAddr)
 	if err := client.HealthCheck(ctx); err != nil {
 		log.Print("Cannot reach User service: ", err)
 	} else {
@@ -28,24 +35,35 @@ func newServer(ctx context.Context, listenAddr, userAddr string) *http.Server {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/rolldice/", http.HandlerFunc(rolldice))
-	mux.Handle("/rolldice/{player}", http.HandlerFunc(rolldice))
+	handle(mux, "/rolldice/", http.HandlerFunc(rolldice))
+	handle(mux, "/rolldice/{player}", http.HandlerFunc(rolldice))
 
 	return &http.Server{
 		Addr:        listenAddr,
 		BaseContext: func(_ net.Listener) context.Context { return ctx },
-		Handler:     mux,
+		Handler:     otelhttp.NewHandler(mux, "/"),
 	}
+}
+
+func handle(mux *http.ServeMux, pattern string, handler http.Handler) {
+	mux.Handle(pattern, otelhttp.WithRouteTag(pattern, handler))
 }
 
 func rolldice(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	tracer := trace.SpanFromContext(ctx).TracerProvider().Tracer(scope)
+	_, span := tracer.Start(r.Context(), "rolldice")
+	defer span.End()
 
 	player := r.PathValue("player")
 
 	client, ok := ctx.Value(userKey).(*user.Client)
 	if !ok {
 		http.Error(w, "Internal Error", http.StatusInternalServerError)
+
+		err := errors.New("no User client")
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return
 	}
 
@@ -53,6 +71,8 @@ func rolldice(w http.ResponseWriter, r *http.Request) {
 		if errors.Is(err, user.ErrInsufficient) {
 			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
 		} else {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
 			http.Error(w, "Internal Error", http.StatusInternalServerError)
 		}
 		return
@@ -60,6 +80,14 @@ func rolldice(w http.ResponseWriter, r *http.Request) {
 
 	roll := 1 + rand.Intn(6)
 
+	if player != "" {
+		span.SetAttributes(attribute.String("player", player))
+	}
+	span.SetAttributes(attribute.Int("roll.value", roll))
+
 	resp := strconv.Itoa(roll) + "\n"
-	_, _ = io.WriteString(w, resp)
+	if _, err := io.WriteString(w, resp); err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+	}
 }
